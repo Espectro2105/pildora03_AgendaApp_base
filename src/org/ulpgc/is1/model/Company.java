da440c3de3336c1d63ebbcb830e8b6433a3fdb2f
Espectro2105 <carlosmv.csc@gmail.com>
2025-09-30 16:01:39 +0100

    Clase company que hereda de la clase Contact. Añadimos primero los atributos asociados a este clase que son name y description. Luego hacemos el constructor pero a este le añadimos el super para heredar los atributos telephone y email de la clase Contact. Posteriormente hacemos el getter y por último el metodo GetName()

--- src/org/ulpgc/is1/model/Company.java
+++ src/org/ulpgc/is1/model/Company.java
@@ -1,5 +1,25 @@
 package org.ulpgc.is1.model;
 
 public class Company extends Contact {
+    private String name;
+    private String description;
 
+    public Company(String telephone, String email, String name, String description) {
+        super(telephone, email);
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
 }
