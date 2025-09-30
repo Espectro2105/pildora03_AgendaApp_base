78f041a6aedf562ddc8c0289e209466d00934c43
Espectro2105 <carlosmv.csc@gmail.com>
2025-09-30 15:56:33 +0100

    Clase Contact con sus atributos, constructor, getter y setter. Añadimos el metodo getName()

--- src/org/ulpgc/is1/model/Contact.java
+++ src/org/ulpgc/is1/model/Contact.java
@@ -1,5 +1,30 @@
 package org.ulpgc.is1.model;
 
 public abstract class Contact {
+    private String telephone;
+    private String email;
+
+    public Contact(String telephone, String email) {
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public abstract String getName();
 
 }
