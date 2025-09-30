38afe715758856564f0202d697f5b5ac1d80dc74
Espectro2105 <carlosmv.csc@gmail.com>
2025-09-30 16:35:42 +0100

    Creo la clase Group en donde tenemos un atributo final (inmutable) y una lista de contactos. Creamos nuestro constructor y el posterior getter y setter. Por último añadimos los metodos addContact() y removeContact()

--- src/org/ulpgc/is1/model/Group.java
+++ src/org/ulpgc/is1/model/Group.java
@@ -1,5 +1,34 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+
 public class Group {
+    private final String name;
+    private ArrayList<Contact> contacts;
+
+    public Group(String name, Contact contacts) {
+        this.name = name;
+        this.contacts = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(ArrayList<Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    public void addContact(Contact contacts) {
+        contacts.add(contacts);
+    }
+
+    public void removeContact(Contact contacts) {
+        contacts.remove(contacts);
+    }
 
 }
